6fa0948a762f67ddaff31042fb5ea4204fb5a73f
Kona Marie <ambermoreyra@gmail.com>
2019-07-21 21:05:21 -0600

    problem solving pulling match object and getting modal to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,11 +23,37 @@ module.exports = function(app) {
 		// req.body hosts is equal to the JSON post sent from the user
 		// This works because of our body parsing middleware
 		var newFriend = req.body;
-		
 		console.log(newFriend);
-		
+
+		// Initialize a best friend score and an empty best friend
+		var matchScore = 40;
+		// When we first get the newFriend, we have no best friend to compare them to
+		var matchIndex = 0;
+
+		// once we have the new friend
+		// we need to compare the friend to our existing list of friends
+		for (var i = 0; i < friends.length; i++) {
+			var friendScore = 0;
+			// friend.scores is an array of integers
+			// so loop through each score to compare to our newFriend's corresponding score
+			for (var j = 0; j < friends[i].scores.length; j++) {
+				var score = parseInt(friends[i].scores[j]);
+				friendScore += Math.abs(score - parseInt(newFriend.scores[j]));
+			}
+
+			// The first time through, match and matchScore will be null
+			// So the first friend we compare will always be the first match
+			if (friendScore < matchScore) {
+				matchIndex = i;
+				matchScore = friendScore;
+			}
+		}
+
+		console.log(friends[matchIndex]);
+		// After all that is done, push our new friend into the list
 		friends.push(newFriend);
 		
-		res.json(newFriend);
+		// Finally, return some info to the ajax request
+		res.json(friends[matchIndex]);
 	}); 
 }
